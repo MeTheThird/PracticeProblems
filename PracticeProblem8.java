be8624f5b0473834c4ca232b943109784db4549a
fatmeany <prabha.krishna@gmail.com>
2016-10-08 12:49:57 -0700

    Create PracticeProblem8.java

---
+++ PracticeProblem8.java
@@ -0,0 +1,15 @@
+package practiceProblems;
+
+public class PracticeProblem8 {
+//Given an integer, figure out if the number is prime
+	
+	public static boolean isPrime(int n)
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
